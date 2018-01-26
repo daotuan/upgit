73c5d61f46212ca25afef21e7a8fb62af0d69dcf
daotuan <daoquantuan30696@gmail.com>
2018-01-26 11:04:13 +0700

    lan1

---
+++ app/src/main/java/lan1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Admin on 1/26/2018.
+ */
+
+public class lan1 {
+    String hoten = "tuan";
+    String hoten1 = "tuan";
+    String hoten2 = "tuan";
+    String hoten3 = "tuan";
+}
